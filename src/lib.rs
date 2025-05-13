1729dc4f9b1138d4a28fa0dbdc99111181240924
Olof <olofwo@kth.se>
2025-05-13 15:51:39 +0200

    added a couple compatibility checkers

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,31 @@ impl Matrix {
 
     pub fn get_cols(&self) -> usize {
         return self.cols;
+    } 
+
+    pub fn check_compatible_addition(&self, m2 : Matrix) -> bool {
+        if self.rows == m2.rows && self.cols == m2.cols {
+            return true
+        } else {
+            return false
+        }
     }
 
-    
+    //checks if matrix multiplication from the right (self is the LHS)
+    pub fn check_compatible_multiplication_lhs(&self, m2 : Matrix) -> bool {
+        if self.cols == m2.rows {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    //checks if matrix multiplication from the left (self is the RHS)
+    pub fn check_compatible_multiplication_rhs(&self, m2 : Matrix) -> bool {
+        if self.rows == m2.cols {
+            return true
+        } else {
+            return false
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use matrix::Matrix;
 
 fn main(){
-    let m = Matrix::initialize(2, 2);
-    println!("Rows: {}, cols: {}", m.get_rows(), m.get_cols());
-    println!("{:?}", m.values);
+    let m1 = Matrix::initialize(2, 2);
+    let m2 = Matrix::initialize(2, 2);
+
+    let booly = m1.check_compatible_addition(m2);
+    println!("Rows: {}, cols: {}", m1.get_rows(), m1.get_cols());
+    println!("{:?}", m1.values);
+    println!("{}", booly);
 }
